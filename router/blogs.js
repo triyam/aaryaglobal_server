4f6845a81783d4882722421a73bed09bedeaf0eb
poornesh <poorneshchenna29@gmail.com>
2022-05-23 20:34:10 +0530

    blogs.js updated -  html file uploads done

--- router/blogs.js
+++ router/blogs.js
@@ -7,6 +7,7 @@ const imageToBase64 = require('image-to-base64')
 const User = require('../model/userSchema')
 const Blog = require('../model/blogSchema')
 const multer = require('multer')
+const fs = require('fs')
 
 const storageEngine = multer.diskStorage({
   destination: function (req, file, cb) {
@@ -24,12 +25,13 @@ router.post(
   upload.single('file'),
   async (req, res) => {
     const { title, description, file } = req.body
-    if (!title || !description) {
-      return res.status(422).json({ error: 'Some data fields are missing' })
-    }
     const type = req.body.type
     try {
       if (type === 'self') {
+        // self written blog
+        if (!title || !description) {
+          return res.status(422).json({ error: 'Some data fields are missing' })
+        }
         const base64FormattedImage = await imageToBase64(req.file.path)
         const blog = await new Blog({
           isSelf: true,
@@ -38,8 +40,18 @@ router.post(
           description: description,
           image: base64FormattedImage,
         }).save()
-      } else {
+      } else if (type === 'html') {
         // HTML file upload
+        if (!title) {
+          return res.status(422).json({ error: 'title is missing' })
+        }
+        const data = fs.readFileSync(req.file.path, 'utf8')
+        const blog = await new Blog({
+          isSelf: false,
+          user_id: req.userID,
+          title: title,
+          html: data,
+        }).save()
       }
     } catch (err) {
       res.status(500).json({ message: 'Internal Server Error' })
